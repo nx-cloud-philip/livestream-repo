aaa3371adccfb4f97ac01986b975f1b15019cdb4
Philip Fulcher <philip@nrwl.io>
2024-08-19 14:04:22 -0600

    another change

--- apps/livestream-repo/app/routes/_index.tsx
+++ apps/livestream-repo/app/routes/_index.tsx
@@ -3,7 +3,7 @@ import NxWelcome from '../nx-welcome';
 export default function Index() {
   return (
     <div>
-      Welcome to the livestream repo
+      Welcome to the livestream repo again
     </div>
   );
 }
