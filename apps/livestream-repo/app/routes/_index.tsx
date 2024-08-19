70e1fcaae21886c42a51e80cf49874bf199999d2
Philip Fulcher <philip@nrwl.io>
2024-08-19 15:05:15 -0600

    changing pipeline trigger

--- apps/livestream-repo/app/routes/_index.tsx
+++ apps/livestream-repo/app/routes/_index.tsx
@@ -3,7 +3,7 @@ import NxWelcome from '../nx-welcome';
 export default function Index() {
   return (
     <div>
-      Welcome to the livestream again after reset
+      Welcome to the livestream trigger tasks
     </div>
   );
 }
